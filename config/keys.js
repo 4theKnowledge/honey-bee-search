4ae74ed713938b1e8224f01595122c8ae5d15198
4theKnowledge <tylerbikaun@gmail.com>
2020-09-07 12:55:52 +0800

    removed cached repo

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://m001-student:m001-mongodb-basics@cluster0.ewkhk.mongodb.net/honey-bee?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,114 @@
+// Import npm packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const app = express();
+const path = require('path');
+
+
+// Body parser iddleware
+app.use(bodyParser.json());
+
+
+
+// db config
+const db = require('./config/keys').mongoURI;
+
+// Make connection to mongodb (localhost or atlas)
+// process.env.MONGODB_URI || 
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Serve static assets (build folder) if we're in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    // Load index.html unless hitting routes
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
+const PORT = process.env.PORT || 5000;
+
+
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose has connected succesfully!');
+}); // event listener for connection event.
+
+
+// Define mongo schema
+const Schema = mongoose.Schema;
+const GeneSeqSchema = new Schema({
+    seq_name: String,
+    seq: String
+    }
+);
+
+// define model
+// register model
+const GeneSeq = mongoose.model('gene-seq-info', GeneSeqSchema);
+
+// saving data to our database
+const data = {
+    seq_name: 'Sequence A',
+    seq: 'aaabbbcccxyz'
+};
+
+const newGeneSeq = new GeneSeq(data);   // instance of the model
+
+
+// newGeneSeq.save((error) => {
+//     if (error) {
+//         console.log('Something went wrong when saving data');
+//     } else {
+//         console.log('Data saved succesfully!');
+//     }
+// });
+
+
+// HTTP request logger
+// Morgan isnt required for MERN but it's a request logger that can
+// be used for debugging... logs requests in terminal.
+app.use(morgan('tiny'));
+
+//Routes
+app.get('/api', (req, res) => {
+
+    // // Get all documents that have aa in their sequence
+    // GeneSeq.find({"seq": {$regex: ".*aa.*"}})
+    // .then((data) => {
+    //     console.log('Data: ', data)
+    //     res.json(data);
+    // })
+    // .catch((error) => {
+    //     console.log('Error: ', error);
+    // });
+
+    res.json({
+        msg: "get request rec."
+    })
+
+});
+
+
+app.post('/api/getseq', (req, res) => {
+    GeneSeq.find({"sequence": {$regex: ".*"+req.body.seq+".*"}}, {'_id': 0})   // doesnt return _id
+    .then((data) => {
+        console.log('Data: ', data)
+        res.json(data);
+    })
+    .catch((error) => {
+        console.log('Error: ', error);
+    });
+
+});
+
+
+app.listen(PORT, console.log(`Server started on ${PORT}`));
